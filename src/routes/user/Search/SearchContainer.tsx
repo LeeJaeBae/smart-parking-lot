892cf7ba5e4271a4968d253eb6a022bd344c93ab
LeeJaeBae <nea4182@gmail.com>
2021-02-02 20:52:02 +0900

    0.1.1 api test

--- src/routes/user/Search/SearchContainer.tsx
+++ src/routes/user/Search/SearchContainer.tsx
@@ -1,11 +1,18 @@
 import SearchPresenter from "./SearchPresenter";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {withRouter} from "react-router-dom";
 import {RouteComponentProps} from 'react-router'
 
 const SearchContainer: React.FC<RouteComponentProps> = ({history}) => {
     const [carId, setCarId] = useState<string>("")
-
+    useEffect(() => {
+        fetch(`http://172.26.2.69:8000/api/checkSpace`).then(r => {
+            console.log(r.json().then(json => {
+                console.log(json)
+            }))
+            console.log(r)
+        })
+    }, [])
     const handleSubmit = (event: React.FormEvent): void => {
         event.preventDefault();
         history.push(`/parking/${carId}`, {carId});
