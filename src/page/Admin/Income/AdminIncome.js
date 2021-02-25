b2bf35e2398171daa4e2f1948cc741e262277ec3
LeeJaeBae <nea4182@gmail.com>
2021-02-25 21:32:31 +0900

    차트 기본 셋업

--- src/page/Admin/Income/AdminIncome.js
+++ src/page/Admin/Income/AdminIncome.js
@@ -1,67 +1,55 @@
 import React, { useState, useEffect } from 'react';
-import { Bar } from 'react-chartjs-2'; // chart.js import
+import { Line } from 'react-chartjs-2'; // chart.js import
 import { getAdminFeeGraph } from '../../../api/admin';
 import './admin_income.css';
 
 import chart from './IncomeChart';
+// import { data } from './IncomeChart';
 
 /**
  * Income
  * @todo api :: 기간 보내주고 -> 차트데이터
  */
 
+const options = {
+	scales: {
+		yAxes: [
+			{
+				ticks: {
+					beginAtZero: true,
+				},
+			},
+		],
+	},
+};
 
-// const options = {
-// 	legend: {
-// 	  display: false, // label 보이기 여부
-// 	},
-// 	scales: {
-// 	  yAxes: [{
-// 		ticks: { 
-// 		  min: 0, // y축 스케일에 대한 최소값 설정
-// 		  stepSize: 1, // y축 그리드 한 칸당 수치
-// 		}
-// 	  }]
-// 	},
-   
 // 	// false : 사용자 정의 크기에 따라 그래프 크기가 결정됨.
 // 	// true : 크기가 알아서 결정됨.
-// 	maintainAspectRatio: false 
-//   }
-  
-//   const data = {
-// 	// 각 막대별 라벨
-// 	labels: ['1번 막대', '2번 막대', '3번 막대'],
-// 	datasets: [
-// 	  {
-// 		borderWidth: 1, // 테두리 두께
-// 		data: [1,2,3], // 수치
-// 		backgroundColor:['yellow','red','green'] // 각 막대 색
-// 	  }
-// 	]
-//   };
+// 	maintainAspectRatio: false
 
+const data = {
+	labels: ['1', '2', '3', '4', '5', '6'],
+	datasets: [
+		{
+			label: '주차요금',
+			data: [12, 19, 3, 5, 2, 3],
+			fill: false,
+			backgroundColor: 'rgb(255, 99, 132)',
+			borderColor: 'rgba(255, 99, 132, 0.2)',
+		},
+	],
+};
 
+////////////////////////
 
-
-
-
-
-
- ////////////////////////
-
-const Income = ( {data} ) => {
-	
+const Income = () => {
 	var start = new Date();
-	const [endDate , setEndDate] = useState(new Date());
-	const [startDate , setStartDate] = useState(new Date());
-	
-	
+	const [endDate, setEndDate] = useState(new Date());
+	const [startDate, setStartDate] = useState(new Date());
+
 	const [feeData, setFeeData] = useState([]);
 	const [total, setTotal] = useState(0);
 	var totalIncome = 0;
-	
-
 
 	// useEffect(() => {
 	// 	getAdminFeeGraph(setFeeData);
@@ -71,47 +59,43 @@ const Income = ( {data} ) => {
 	useEffect(() => {
 		console.log(feeData);
 		// setTotal(0);
-		feeData.length !== 0 && feeData.map((fee) => 
-		totalIncome += fee.fee_day
-		);
+		feeData.length !== 0 && feeData.map((fee) => (totalIncome += fee.fee_day));
 		console.log(totalIncome);
-		setTotal(totalIncome)
+		setTotal(totalIncome);
 	}, [feeData]);
 
-	
 	// 지정 기간에 따라 변수 값 변경
 	const setPeriod = (p) => {
 		var period = 0;
 		// e.preventDefault();
 		// var specifiedDuration = 1;
-		console.log("period : " + p);
-		switch(p) {
-			case "today" :
+		console.log('period : ' + p);
+		switch (p) {
+			case 'today':
 				period = 1;
 				break;
-			case "week" :
+			case 'week':
 				period = 7;
 				break;
-			case "1month" :
+			case '1month':
 				period = 30;
 				break;
-			case "3month" :
+			case '3month':
 				period = 90;
 				break;
-			case "6month" :
+			case '6month':
 				period = 180;
 				break;
-			case "1year" :
+			case '1year':
 				period = 800;
 				break;
-			default :
+			default:
 				period = 1;
 		}
 
 		// console.log(specifiedDuration);
 		return period;
-	}
-
+	};
 
 	// 수입 데이터 가져오기
 	const getFeeData = (e) => {
@@ -121,18 +105,14 @@ const Income = ( {data} ) => {
 		const period = setPeriod(periodText);
 		// var duration = setPeriod();
 		// console.log(new Date());
-		start = new Date(endDate - (1000 * 60 * 60 * 24) * period); //
-		
+		start = new Date(endDate - 1000 * 60 * 60 * 24 * period); //
+
 		// console.log("시작 : " + start);
 		// console.log("끝 : " + endTime);
 
-
-		getAdminFeeGraph(setFeeData , start , endDate);
+		getAdminFeeGraph(setFeeData, start, endDate);
 		// setStartTime(start);
-
-	}
-
-
+	};
 
 	return (
 		<>
@@ -141,9 +121,9 @@ const Income = ( {data} ) => {
 				<div className='setPeriod'>
 					<div className='income_form'>
 						{/* 지정 날짜 기간 계산해서 넘기기 */}
-						<button onClick={getFeeData} className='income_btn' value='today' name='period'>
+						{/* <button onClick={getFeeData} className='income_btn' value='today' name='period'>
 							오늘
-						</button>
+						</button> */}
 						<button onClick={getFeeData} className='income_btn' value='week' name='period'>
 							1주일
 						</button>
@@ -159,19 +139,23 @@ const Income = ( {data} ) => {
 						<button onClick={getFeeData} className='income_btn' value='1year' name='period'>
 							1년
 						</button>
+						<input
+							className='income_btn'
+							type='date'
+							style={{ width: '160px' }}
+							defaultValue={new Date().toISOString().substring(0, 10)}
+							onChange={(e) => {
+								setEndDate(new Date(e.target.value));
+							}}
+						/>
 					</div>
 				</div>
-				<div class='income'>총 수입 {total}원</div>
-				<br></br>
+				<div className='income'>총 수입 {total}원</div>
+				<br />
 			</div>
 			{/* 차트 */}
 			<div className='chart'>
-				<canvas>
-					<script>
-						<chart data={data}/>
-					</script>
-				</canvas>
-
+				<Line id='test' data={data} options={options} />
 			</div>
 		</>
 	);
