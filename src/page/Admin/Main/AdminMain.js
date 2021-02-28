7b1f4d7ea5b915e6bcf2fc4444ee613172361eba
LeeJaeBae <nea4182@gmail.com>
2021-02-28 19:35:25 +0900

    react-videoroom-janus is installed

--- src/page/Admin/Main/AdminMain.js
+++ src/page/Admin/Main/AdminMain.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { getAdminLiveSituation } from '../../../api/admin';
 import './admin_main.css';
 
-
 const style = {
 	display: 'inline-block',
 	width: '100%',
